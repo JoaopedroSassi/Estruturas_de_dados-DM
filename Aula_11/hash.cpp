ab20b69205d7179c1a54704dd5924ab14edbb1ec
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-12-20 17:39:34 -0300

    Add exercises class 11

---
+++ Aula_11/hash.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include "hash.h"
+
+using namespace std;
+
+   int Hash::funcaoHash(Aluno aluno)
+   {
+      return (aluno.obterRa() % max_posicoes);
+   }
+
+   Hash::Hash(int tam_vetor, int max)
+   {
+      quant_itens = 0;
+      max_itens = max;
+      max_posicoes = tam_vetor;
+      estrutura = new Aluno[tam_vetor];
+   }
+
+   Hash::~Hash()
+   {
+      delete [] estrutura;
+   }
+
+   bool Hash::estaCheio()
+   {
+      return (quant_itens == max_itens);
+   }
+
+   int Hash::obterTamanho()
+   {
+      return (quant_itens);
+   }
+
+   void Hash::push(Aluno aluno)
+   {
+      if (estaCheio())
+      {
+         cout << "A tabela hash está cheia!\n";
+      } else {
+         int local = funcaoHash(aluno);
+         while (estrutura[local].obterRa() > 0){
+            local = (local + 1) % max_posicoes;   
+         }
+         estrutura[local] = aluno;
+         quant_itens++;
+      }
+   }
+
+   void Hash::pop(Aluno aluno)
+   {
+      int local = funcaoHash(aluno);
+      bool test = false;
+      while (estrutura[local].obterRa() != -1){
+         if (estrutura[local].obterRa() == aluno.obterRa()){
+            cout << "Elemento removido!\n";
+            estrutura[local] = Aluno(-2, " ");
+            quant_itens--;
+            test = true;   
+            break;
+         }
+         local = (local + 1) % max_posicoes;
+      }
+      if (!test){
+         cout << "Elemento não encontrado!\n";
+      }
+      
+   }
+
+   void Hash::search(Aluno &aluno, bool &busca)
+   {
+      int local = funcaoHash(aluno);
+      busca = false;
+      while (estrutura[local].obterRa() != -1){
+         if (estrutura[local].obterRa() == aluno.obterRa()){
+            busca = true;
+            aluno = estrutura[local];
+            break;
+         }
+         local = (local + 1) % max_posicoes;
+      }
+   }
+
+   void Hash::print()
+   {
+      cout << "Tabela hash:\n";
+      for (int i = 0; i < max_posicoes; i++)
+      {
+         if (estrutura[i].obterRa() > 0)
+         {
+            cout << i << ":" << estrutura[i].obterRa() << "|" << estrutura[i].obterNome();
+         }
+      }
+   }
+
---
+++ Aula_11/main_hash.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "hash.h"
+
+using namespace std;
+
+int main(){
+   cout << "Programa gerador de hash (com colisão)!\n";
+
+   cout << "Digite o tamanho da hash:";
+   int tam_vet;
+   cin >> tam_vet;
+
+   cout << "Digite o número máximo de elementos:";
+   int max;
+   cin >> max;
+
+   cout << "Fator de carga: " << float(max) / float(tam_vet) << endl;
+
+   Hash alunoHash(tam_vet, max);
+
+   int ra = 0, opcao = 0;
+   string nome;
+   bool busca;
+
+   do
+   {
+      cout << "0 - Parar o algoritmo\n";
+      cout << "1 - Inserir um elemento\n";
+      cout << "2 - Remover um elemento\n";
+      cout << "3 - Buscar um elemento\n";
+      cout << "4 - Imprimir a hash\n";
+      cin >> opcao;
+
+      if (opcao == 1){
+         cout << "Digite o RA do aluno:";
+         cin >> ra;
+         cout << "Digite o nome do aluno:";
+         cin >> nome;
+         Aluno aluno(ra, nome);
+         alunoHash.push(aluno);
+      } else if (opcao == 2){
+         cout << "Digite o RA a ser inserido:";
+         cin >> ra;
+         Aluno aluno(ra, " ");
+         alunoHash.pop(aluno);
+      } else if (opcao == 3){
+         cout << "Digite o RA a ser buscado:";
+         cin >> ra;
+         Aluno aluno(ra, " ");
+         alunoHash.search(aluno, busca);
+         if (busca){
+            cout << "Aluno encontrado!\n";
+            cout << "RA: " << aluno.obterRa() << endl;
+            cout << "Nome:" << aluno.obterNome() << endl;
+         } else {
+            cout << "ERRO | Aluno não encontrado!";
+         }
+      } else if (opcao == 4){
+         alunoHash.print();
+      }
+   } while (opcao != 0);
+   
+   return 0;
+}
